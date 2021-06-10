f887bdd7832e4e057f571b30865a83a7215d0f3d
Miguel Luque <migue.luque.2014@gmail.com>
2021-06-10 13:25:42 -0500

    Clase 3 - Múltiples Peticiones a un API con Callbacks

--- src/callback/challenge.js
+++ src/callback/challenge.js
@@ -1,17 +1,40 @@
 let XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
 
+let API = 'https://rickandmortyapi.com/api/character/';
+
 function fetchData(url_api, callback) {
     let xhttp = new XMLHttpRequest();
-    xhttp.open('GET', url_api, async=true);
+    xhttp.open('GET', url_api, true);
     xhttp.onreadystatechange = function (event) { 
         if (xhttp.readyState === 4){
             if(xhttp.status === 200){
                 callback(null, JSON.parse(xhttp.responseText))
             } else {
-                const error = new Error(`Error ${url_api}`)
+                let error = new Error("Error: " + url_api)
                 return (error, null)
             }
         }
     }
     xhttp.send();
 }
+
+
+fetchData(API, (error1, data1) => {
+    if(error1) return console.error(error1);
+    fetchData(API + data1.results[0].id, (error2, data2) => {
+        if(error2) return console.error(error2);
+        fetchData(data2.origin.url, (error3, data3) => {
+            if(error3) return console.error(error3);
+            // mostramos los resultados :) 
+            console.log(data1.info.count);
+            console.log(data2.name);
+            console.log(data3.dimension);
+
+            console.log()
+            // rutas de las peticiones en orden
+            console.log(API);
+            console.log(API + data1.results[0].id); 
+            console.log(data2.origin.url); ;
+        })
+    })
+})
